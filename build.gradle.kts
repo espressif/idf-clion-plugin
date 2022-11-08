7d23e278fcadeaa0b0df70d61aa6dfd0eec60461
alirana01 <ali.azamrana@espressif.com>
2022-11-08 12:50:55 +0100

    clion configs for the plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.3.3")
-    type.set("IC") // Target IDE Platform
+    version.set("2022.2.4")
+    type.set("CL") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
 }
